9cbc85a933144efa355ae3152e922b981bf81dfc
sheng411 <jerry65855@gmail.com>
2022-06-13 22:05:58 +0800

    學長教(更一)

--- C++/pg0503_11003058A.cpp
+++ C++/pg0503_11003058A.cpp
@@ -1,61 +1,86 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int fre=20;
+int fre=5;
 
 class Mystudent{
     private:
-        int i;
-        float f;
-        double d;
-        string s;
         string myname;
         int math;
         int eng;
         float average;
     public:
-        void student();
+    Mystudent(string na,int ma,int en,float ave);
+    string getmyname(){return myname;}
+    int getmymath(){return math;}
+    int getmyeng(){return eng;}
+    float getmyave(){return average;}
 };
 
 template <class f>
-    f findmax(f meam){
+    void myoutput(vector <f> out){
+        for(int i=0;i<fre;i++){
+            cout<<out[i]<<endl;
+        }
+    }
+
+template <class f>
+    void findmax(vector <f> meam){
         int max=0;
         f maax;
         for(int i=0;i<fre;i++){
             if(meam[i]>meam[max]){
-                //maax=meam[i];
+                maax=meam[i];
                 max=i;
             }
         }
-        /*for(int i=0;i<fre;i++){
-            cout<<meam[i]<<endl;
-        }*/
-        cout<<"\n\ntop-->"<<"index="<<max;
+        cout<<"\ntop-->"<<"index="<<max<<"--"<<maax;
     }
 
-void Mystudent::student(){
-    vector <Mystudent> rw;
-    Mystudent test;
+void student(vector <Mystudent>&rw){
     srand(time(NULL));
     for(int i=0;i<fre;i++){
-        test.i=rand()%100;
-        test.f=float(rand()%100);
-        test.d=double(rand()%100);
-        test.s=char(rand()%26)+1;
         string na="aaaaaa";
         for(int x=0;x<6;x++){
             na[x]+=rand()%26;
         }
-        test.myname=na;
-        test.math=(rand()%(8-4+1))+4;
-        test.eng=(rand()%(8-4+1))+4;
-        test.average=(math+eng)/2;
+        int ma=(rand()%(8-4+1))+4;
+        int en=(rand()%(8-4+1))+4;
+        float ave=(ma+en)/2;
+        Mystudent test(na,ma,en,ave);
         rw.push_back(test);
     }
 }
 
+Mystudent::Mystudent(string na,int ma,int en,float ave){
+    myname=na;
+    math=ma;
+    eng=en;
+    average=ave;
+}
+
 int main(){
     vector <Mystudent> rw;
-    findmax(rw);
+    vector <int> i;
+    vector <float> f;
+    vector <double> d;
+    vector <string> s;
+    for(int j=0;j<fre;j++){
+        i.push_back(rand()%100);
+        f.push_back(rand()%100);
+        d.push_back(rand()%100);
+        string na="aaaaaa";
+        for(int x=0;x<6;x++){
+            na[x]+=rand()%26;
+        }
+        s.push_back(na);
+    }
+
+    student(rw);
+    
+    myoutput <int>(i);
+    findmax <int>(i);
+
+
     return 0;
 }
