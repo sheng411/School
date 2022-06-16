2550ddd81cd9dc1ae8e5b274192c39d053dbe3b5
sheng411 <jerry65855@gmail.com>
2022-06-16 17:20:21 +0800

    一次優化

--- python/projest_web_crawler.py
+++ python/projest_web_crawler.py
@@ -9,7 +9,7 @@ tm=time.localtime()
 print(tm)   #查看所有時間
 
 #爬蟲
-new=[]
+#new=[]
 url="https://www.ettoday.net/news/realtime-hot.htm" #ETtoday即時人氣
 request=req.Request(url,headers={
     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"
@@ -18,16 +18,15 @@ with req.urlopen(request) as response:
     aa_data=response.read().decode("utf-8")
 root=bs4.BeautifulSoup(aa_data,"html.parser")
 titles=root.find_all("div",class_="piece clearfix")
-titles_cont=root.find_all("p",class_="summary")
 titles_num=len(root.find_all("em",class_="number"))
-print(titles_num)
+print("總報導數:",titles_num,"\n")
 
 
 fre=int(input("所需新聞量:"))   #用輸入改掉
 cont=str(input("是否需要顯示內文(y/n):"))
 if fre>titles_num:             #輸入數大於新聞量則使用新聞最大量
     fre=titles_num
-print("**************")
+print("*******************")
 
 a=1
 for tit in titles:
@@ -38,12 +37,12 @@ for tit in titles:
         a+=1
         #new=tit.append
         #print(new)
-
+print("*******************")
 
 #tkinter
 window=Tk()
 window.title("ETtoday熱門新聞")
-c=Canvas(window,width=500,height=500,bg="MidnightBlue") #紫色
+c=Canvas(window,width=500,height=500,bg="MidnightBlue")
 c.pack()
 
 
