78ba7ac309f86b14175657b4e39427806d840749
sheng411 <jerry65855@gmail.com>
2022-06-20 11:26:23 +0800

    考試(1)

---
+++ C++/0620/fe1_3058_0620_p1.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void f(int a){
+    cout<<"輸入學號:";
+    cin>>a;
+    a=sqrt(a)*10;
+    cout<<"11003058A_林祐陞-"<<a;
+}
+
+int main(){
+    int a;
+    f(a);
+    return 0;
+}
