20db71805a9ada40ae465672186570a6654c3eb7
sheng411 <jerry65855@gmail.com>
2022-10-02 03:03:15 +0800

    add bubble sort

--- C++/data_structure_course/001.cpp
+++ C++/data_structure_course/001.cpp
@@ -1,8 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
+int fre=0;  //quantity of data
+
+int ins(int in[],int n){
+
+}
+
+
+int bub(int b[],int n){     //b is list ,n is list size.
+    int i,j,k;
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-1-i;j++){
+            if(b[j]>b[j+1]){
+                swap(b[j],b[j+1]);
+            }
+        }
+    }
+    cout<<"list-->"<<b<<endl;
+    return 0;
+}
+
+
+
+//sel(int s[],int)
 
 int main(){
-    int a;
+    srand(time(NULL));
+    int len;
+    
+    len=a.size();
+    cout<<"len-->"<<len<<endl;
     
     return 0;
 }
