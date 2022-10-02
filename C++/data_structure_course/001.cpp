3f4f1f6bea5e0d21254e199076ca902027bfcc06
sheng411 <jerry65855@gmail.com>
2022-10-02 22:52:24 +0800

    更新排序法寫法

--- C++/data_structure_course/001.cpp
+++ C++/data_structure_course/001.cpp
@@ -4,34 +4,26 @@ using namespace std;
 int fre=10;       //quantity of data
 int smfre=0;     //Use of small data
 
-/*
+
 template <class ins>
     ins is(ins in,int n){
-        int temp,j=0;
-        double sta,end,ti;
-        sta=clock();
+        int temp;
+        int j=0;
         for(int i=0;i<n;i++){
             temp=in[i];
             j=i-1;
-            while(temp<in[j] && j>=0){
-                in[j+1]=in[j-1];
+            while((temp<in[j]) && (j>=0)){
+                in[j+1]=in[j];
                 j--;
             }
             in[j+1]=temp;
         }
-        end=clock();
-        ti=(end-sta)/1000;
-        cout<<fre<<"data"<<endl;
-        cout<<"insertion list-->"<<in<<endl;
-        //cout<<"usage time-->"<<ti<<"s"<<endl;
         return in;
     }
 
 template <class se>
     se sel(se s,int n){
-        double sta,end,ti;
-        sta=clock();
-        for(int i=0;i<n-1;i++){
+        for(int i=0;i<n;i++){
             int mins=i;
             for(int j=i+1;j<n;j++){
                 if(s[j]<s[mins]){
@@ -42,15 +34,9 @@ template <class se>
                 swap(s[i],s[mins]);
             }
         }
-        end=clock();
-        ti=(end-sta)/1000;
-        cout<<fre<<"data"<<endl;
-        cout<<"insertion list-->"<<s<<endl;
-        //cout<<"usage time-->"<<ti<<"s"<<endl;
         return s;
     }
 
-*/
 template <class bu>
     bu bub(bu b,int n){     //b is list ,n is list size.
         int i,j,k;
@@ -94,7 +80,6 @@ template <class mers>
             mergsort(m,mid+1,enr);
             merg(m,snr,mid,enr);
         }
-        /*
         cout<<"merge list-->"<<endl;
         for(int c=0;c<fre;c++){
             cout<<m[c]<<"\t";
@@ -139,17 +124,23 @@ int main(){
     sta=clock();
     if(smfre!=0){
         for(int i=0;i<smfre;i++){
-            //123
+            sel(a,len);
         }
     }
     else{
-        bub(a,len);
+        sel(a,len);
+
     }
     end=clock();
     //timing end
-    ti=(end-sta)/1000;
-    output(bub(a,len));
+    if(smfre!=0){
+        ti=(end-sta)/smfre;
+    }
+    else{
+        ti=(end-sta);
+    }
+    output(sel(a,len));
     cout<<"\nlen-->"<<len<<endl;
-    cout<<"usage time-->"<<ti<<"s"<<endl;
+    cout<<"usage time-->"<<ti<<"ms"<<endl;
     return 0;
 }
