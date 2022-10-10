1cabd62e26b6de4aa6b2d10f070e2b1e0bef5545
sheng411 <jerry65855@gmail.com>
2022-10-11 01:36:36 +0800

    無法測試merge

--- C++/data_structure_course/test.cpp
+++ C++/data_structure_course/test.cpp
@@ -1,6 +1,85 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int fre=10;
+
+
+
+
+template <class mer>
+    void merg(mer mr,int sn,int md,int en){
+        int l1=md-sn+1;
+        int r1=en-md;
+        mer L[l1],R[r1];
+        for(int i=0;i<l1;i++){
+            L[i]=mr[sn+i];
+        }
+        for(int j=0;j<r1;j++){
+            R[j]=mr[md+1+j];
+        }
+        int i=0,j=0,k=sn;
+        while(i<l1 && j<r1){
+            if(L[i] <=R[j]){
+                mr[k]=L[i];
+                i++;
+            }
+            else{
+                mr[k]=R[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<l1){
+            mr[k]=L[i];
+            i++;
+            k++;
+        }
+        while(j<r1){
+            mr[k]=R[j];
+            j++;
+            k++;
+        }
+    }
+
+template <class mes>
+    void mergso(mes ms,int st,int ed){
+        if(st<ed){
+            int md=(st+ed)/2;
+            mergso(ms,st,md);
+            mergso(ms,md+1,ed);
+            merg(ms,st,md,ed);
+        }
+    }
+
 int main(){
-    
+    vector <string>vs;
+    vector <int>vi;
+    vector <long>vlo;
+    vector <double>vd;
+    vector <float>vf;
+
+    srand(time(NULL));
+    int rdn=1000;
+    for(int i=0;i<fre;i++){
+        vi.push_back(rand()%rdn);
+        vlo.push_back(rand()%rdn);
+        vd.push_back(rand()%rdn);
+        vf.push_back(rand()%rdn);
+        string na="aaaaaa";
+        for(int x=0;x<6;x++){
+            na[x]+=rand()%26;
+        }
+        vs.push_back(na);
+    }
+
+    for(int i=0;i<fre;i++){
+        cout<<vi[i]<<"\t";
+    }
+
+    mergso(vi,0,fre);
+
+    for(int i=0;i<fre;i++){
+        cout<<vi[i]<<"\t";
+    }
+    return 0;
 }
