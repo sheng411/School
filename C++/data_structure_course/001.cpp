ebe0c4ac00702b1d6f0521878795764b36669326
sheng411 <jerry65855@gmail.com>
2022-11-04 02:51:27 +0800

    Question-->rand無法單獨做成function

--- C++/data_structure_course/001.cpp
+++ C++/data_structure_course/001.cpp
@@ -5,9 +5,9 @@ int fre=1000;       //quantity of data
 long double smfre=100000;     //Use of small data
 
 /*
-input 1--> is sel bub merg 
+input 1--> ins sel bub merg 
 
-更改時間:2022/10/26 15:50
+更改時間:2022/11/04 02:51
 */
 
 template <class ins>
@@ -55,53 +55,6 @@ template <class bu>
         return b;
     }
 
-template <class mer>
-    mer merg(mer &mr,int sn,int md,int en){
-        using elType = typename mer::value_type;
-        int L_s=md-sn+1;
-        int R_s=en-md;
-        elType L[L_s],R[R_s];
-        for(int ll=0;ll<L_s;ll++){
-            L[ll]=mr[sn+ll];
-        }
-        for(int rr=0;rr<R_s;rr++){
-            R[rr]=mr[md+1+rr];
-        }
-        int i=0,j=0,k=sn;
-        while(i<L_s && j<R_s){
-            if(L[i] <=R[j]){
-                mr[k]=L[i];
-                i++;
-            }
-            else{
-                mr[k]=R[j];
-                j++;
-            }
-            k++;
-        }
-        while(i<L_s){
-            mr[k]=L[i];
-            i++;
-            k++;
-        }
-        while(j<R_s){
-            mr[k]=R[j];
-            j++;
-            k++;
-        }
-        return mr;
-    }
-
-template <class mes>
-    mes mergso(mes &ms,int st,int ed){
-        if(st<ed){
-            int md=(st+ed)/2;
-            mergso(ms,st,md);
-            mergso(ms,md+1,ed);
-            merg(ms,st,md,ed);
-        }
-        return ms;
-    }
 
 template <class out>
     void output(out st){
@@ -146,45 +99,8 @@ template <class go>
         myfile.close();
     }
 
-
-int main(){
-    cout<<"start"<<endl;
-    
-    string *as=new string[fre];
-    int *ai=new int[fre],pi;
-    long *alo=new long[fre],plo;
-    double *ad=new double[fre],pd;
-    float *af=new float[fre],pf;
-    
-    vector <string>vs;
-    vector <int>vi;
-    vector <long>vlo;
-    vector <double>vd;
-    vector <float>vf;
-    
-    
-    array <string,1000> aas;
-    array <int,1000> aai;
-    array <long,1000> aalo;
-    array <double,1000> aad;
-    array <float,1000> aaf;
-    
-
-
-    // open file
-    fstream myfile;
-    myfile.open("count.txt",ios::out);      //改app
-    myfile<<"sort\t\t";
-    myfile<<"type\t\t";
-    myfile<<"time\t\t";
-    myfile<<"data num\t";
-    myfile<<"rep ti\t\t";   //repeat time
-    myfile<<"data str\n\n";
-    myfile.close();
-    
-    cout<<"file 200OK"<<endl;
-
-    
+/*
+void randd(int &vi,long &vlo,double &vd,float &vf){
     srand(time(NULL));
     int rdn=10,rdm=10000;
 
@@ -215,6 +131,78 @@ int main(){
         aad[i]=rand()%rdm/double(rdn);
         aaf[i]=rand()%rdm/float(rdn);
     }
+}*/
+
+
+int main(){
+    cout<<"start"<<endl;
+    
+    string *as=new string[fre];
+    int *ai=new int[fre],pi;
+    long *alo=new long[fre],plo;
+    double *ad=new double[fre],pd;
+    float *af=new float[fre],pf;
+    
+    vector <string>vs;
+    vector <int>vi;
+    vector <long>vlo;
+    vector <double>vd;
+    vector <float>vf;
+    
+    
+    array <string,1000> aas;
+    array <int,1000> aai;
+    array <long,1000> aalo;
+    array <double,1000> aad;
+    array <float,1000> aaf;
+    
+
+
+    // open file
+    fstream myfile;
+    myfile.open("count.txt",ios::out);      //改app /out
+    myfile<<"sort\t\t";
+    myfile<<"type\t\t";
+    myfile<<"time\t\t";
+    myfile<<"data num\t";
+    myfile<<"rep ti\t\t";   //repeat time
+    myfile<<"data str\n\n";
+    myfile.close();
+    
+    cout<<"file 200OK"<<endl;
+
+    /*
+    srand(time(NULL));
+    int rdn=10,rdm=10000;
+
+    for(int i=0;i<fre;i++){
+        vi.push_back(rand()%rdm/int(rdn));
+        vlo.push_back(rand()%rdm/long(rdn));
+        vd.push_back(rand()%rdm/double(rdn));
+        vf.push_back(rand()%rdm/float(rdn));
+        string na="aaaaaa";
+        for(int x=0;x<6;x++){
+            na[x]+=rand()%26;
+        }
+        vs.push_back(na);
+
+        pi=rand()%rdm/int(rdn);
+        plo=rand()%rdm/long(rdn);
+        pd=rand()%rdm/double(rdn);
+        pf=rand()%rdm/float(rdn);
+        as[i]=na;
+        ai[i]=pi;
+        alo[i]=plo;
+        ad[i]=pd;
+        af[i]=pf;
+
+        aas[i]=na;
+        aai[i]=rand()%rdm/int(rdn);
+        aalo[i]=rand()%rdm/long(rdn);
+        aad[i]=rand()%rdm/double(rdn);
+        aaf[i]=rand()%rdm/float(rdn);
+    }*/
+
 
     //sort choose
     int sc;
@@ -273,23 +261,6 @@ int main(){
         gogo(bub(aalo,fre),2,2,2);
         gogo(bub(aad,fre),2,3,2);
         gogo(bub(aaf,fre),2,4,2);
-        break;
-    case 4:
-        gogo(mergso(as,0,fre-1),3,0,0);
-        gogo(mergso(ai,0,fre-1),3,1,0);
-        gogo(mergso(alo,0,fre-1),3,2,0);
-        gogo(mergso(ad,0,fre-1),3,3,0);
-        gogo(mergso(af,0,fre-1),3,4,0);
-        gogo(mergso(vs,0,fre-1),3,0,1);
-        gogo(mergso(vi,0,fre-1),3,1,1);
-        gogo(mergso(vlo,0,fre-1),3,2,1);
-        gogo(mergso(vd,0,fre-1),3,3,1);
-        gogo(mergso(vf,0,fre-1),3,4,1);
-        gogo(mergso(aas,0,fre-1),3,0,2);
-        gogo(mergso(aai,0,fre-1),3,1,2);
-        gogo(mergso(aalo,0,fre-1),3,2,2);
-        gogo(mergso(aad,0,fre-1),3,3,2);
-        gogo(mergso(aaf,0,fre-1),3,4,2);
         break;
     default:
         cout<<"404 error"<<endl;
