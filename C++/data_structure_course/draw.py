c9a850671a04193e02939c1268d56c7ea059c524
sheng411 <jerry65855@gmail.com>
2022-11-21 00:46:00 +0800

    修正

--- C++/data_structure_course/draw.py
+++ C++/data_structure_course/draw.py
@@ -1,8 +1,10 @@
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+data=[]
 
 #讀檔案
+#blue is C array,red is vector
 
 path = 'D:/code/School/C++/data_structure_course/read.txt'
 f = open(path, 'r')
@@ -13,41 +15,82 @@ for i in range(15):
     for line in f:
         text[i][j]=line.split(" ")
         print(text[i][j],"\n")  #line,
+        
+        data.append(text[i][j][2])
         j+=1
+        
 
 f.close()
 
+nptis=str(text[0][0][0])+" sort-->"+str(text[0][0][3])+","+str(text[0][0][4])
+print(nptis)
 
-
+#print(*data,type(data[0]))
+'''
 sn=int(input("start:"))
 en=int(input("end:"))
 print('hi',sn,en)
+
+
+for i in range(sn,en):
+    data.append(int(input(i)))
 '''
-data=[]
-for i in range(sn):
-    data.append(int(input()))
+data=list(map(int,data))
+p1=[]
+for i in range(5):
+    p1.append(data[i])
+print(p1)
 
+p2=[]
+for i in range(5,10):
+    p2.append(data[i])
+print(p2)
 
-x = np.array([1, 5])
-y = np.array([20,40])
+'''
+p3=[]
+for i in range(10,15):
+    p3.append(data[i])
+print(p3)
+'''
+pn=['str','int','long','double','float']
+
+k=0
+for f in range(5):
+    p1n=pn[f]
+    x = np.array([f+1])
+    y = np.array(p1[f])
+    plt.plot(x, y, 'o-b',label=p1n)
+    
+#plt.legend(title = "Data type:") #線條標籤
+
+for f in range(5):
+    p2n=pn[f]
+    x1= np.array([f+1])
+    y1= np.array(p2[f])
+    plt.plot(x1, y1, 'o-r',label=p2n)
+    
+'''
+for f in range(5):
+    p3n=pn[f]
+    x= np.array([f+1])
+    y= np.array(p3[f])
+    plt.plot(x, y, 'o-g',label=p3n)
+'''
+
+#變直線
+
+'''
+p2n='test2'
 x1 = np.array([-10,0])
 y1 = np.array([0, 80])
-
-p1n='test1'
-p2n='test2'
-
-plt.plot(x, y)
-plt.plot(x, y, 'o-',label=p1n)
-
 plt.plot(x1, y1)
 plt.plot(x1, y1, 'o-',label=p2n)
-
+'''
 
 #plt.bar(x,y)    #長條圖
 plt.grid(True)  #網格
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.title('Sort')
-plt.legend(title = "Four animals:") #線條標籤
+plt.xlabel('Type')
+plt.ylabel('Time')
+plt.title(nptis)
+
 plt.show()
-'''
