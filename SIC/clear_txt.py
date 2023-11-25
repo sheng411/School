fc15b30f142ab519b81c67857f7c46ed8bff83bd
sheng411 <jerry65855@gmail.com>
2023-11-25 18:41:28 +0800

    Add function to clear txt file

---
+++ SIC/clear_txt.py
@@ -0,0 +1,32 @@
+from time import *
+
+#time
+def t_ime(a):
+    t=localtime(a)
+    Y=t.tm_year
+    M=t.tm_mon
+    D=t.tm_mday
+    h=t.tm_hour
+    m=t.tm_min
+    s=t.tm_sec
+    print(Y,"/",M,"/",D,"  ",h,":",m,":",s,"\n")
+
+try:
+    hash_txt = open('School/SIC/hash.txt', 'w')
+    output_txt = open('School/SIC/output.txt', 'w')
+    tree_txt = open('School/SIC/tree.txt', 'w')
+
+    hash_txt.write("")
+    output_txt.write("")
+    tree_txt.write("")
+
+    print("\n* Clear all txt file !!")
+    t_ime(time())
+
+except Exception as e:
+    print(f"Error{e}")
+
+finally:
+    hash_txt.close()
+    output_txt.close()
+    tree_txt.close()
