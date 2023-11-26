13e930b6e2b91483571542d52b3898d688b43a02
sheng411 <jerry65855@gmail.com>
2023-11-27 01:05:59 +0800

    add easy test area

---
+++ SIC/easy_sample/clear_txt.py
@@ -0,0 +1,33 @@
+from time import *
+
+
+
+#time
+def t_ime(a):
+    t=localtime(a)
+    Y=t.tm_year
+    M=t.tm_mon
+    D=t.tm_mday
+    h=t.tm_hour
+    m=t.tm_min
+    s=t.tm_sec
+    print(Y,"/",M,"/",D,"  ",h,":",m,":",s,"\n")
+
+try:
+    hash_txt = open('D:/code/School/SIC/easy_sample/hash.txt', 'w')
+    output_txt = open('D:/code/School/SIC/easy_sample/output.txt', 'w')
+    tree_txt = open('D:/code/School/SIC/easy_sample/tree.txt', 'w')
+
+    hash_txt.write("")
+    output_txt.write("")
+    tree_txt.write("")
+
+except Exception as e:
+    print(f"Error{e}")
+
+finally:
+    hash_txt.close()
+    output_txt.close()
+    tree_txt.close()
+    print("\n* Clear all txt file !!")
+    t_ime(time())
